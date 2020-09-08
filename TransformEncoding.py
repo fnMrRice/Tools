ebd9228b06d1461947657e2f1a69dedea99c4763
fnMrRice <doraismydora@gmail.com>
2020-09-08 12:39:14 +0800

    update: updated python code with 'with'

--- TransformEncoding.py
+++ TransformEncoding.py
@@ -2,40 +2,43 @@
 from chardet import detect
 import os
 
+
 def trans(txtFile):
     if not os.path.isfile(txtFile):
         raise RuntimeError('PathError')
-        return False
-    fp=open(txtFile,'rb')
-    cont=fp.read()
-    if detect(cont)['confidence']<0.9:
-        raise RuntimeError('FileError')
-        return False
-    
-    fp.close()
+    cont = None
+    with open(txtFile, 'rb') as fp:
+        cont = fp.read()
+        if detect(cont)['confidence'] < 0.9:
+            raise RuntimeError('FileError')
 
-    fp=open(txtFile+'.bak','wb')
-    fp.write(cont)
-    fp.close()
+    os.rename(txtFile, txtFile+'.bak')
+    # fp = open(txtFile+'.bak', 'wb')
+    # fp.write(cont)
+    # fp.close()
 
-    cont=cont.decode(detect(cont)['encoding']).encode('utf8')
+    cont = cont.decode(detect(cont)['encoding']).encode('utf8')
 
-    fp=open(txtFile,'wb')
-    fp.write(cont)
-    fp.close()
+    with open(txtFile, 'w+', encoding='utf-8') as fp:
+        fp.write(cont)
+    # fp = open(txtFile, 'wb')
+    # fp.write(cont)
+    # fp.close()
 
-def transDir(file_dir,subdir=False):
+
+def transDir(file_dir, subdir=False):
     if not os.path.isdir(file_dir):
         raise RuntimeError('PathError')
-    for root,dirs,files in os.walk(file_dir):
+    for root, dirs, files in os.walk(file_dir):
         for f in files:
-            fPath=os.path.join(root,f)
+            fPath = os.path.join(root, f)
             try:
                 trans(fPath)
                 print(fPath)
             except RuntimeError as e:
                 print(e+': '+fPath)
-        #print(files)
+        # print(files)
 
-if '__main__'==__name__:
+
+if '__main__' == __name__:
     transDir('E:\\GameUnpack\\まいてつ\\data\\scenario')
